501c5adef4315aeb62bf0b5e425d9e38f00dd4d4
Daniel Garrido <dgarrido@plainconcepts.com>
2021-01-29 15:56:08 +0100

    Application of good practices in variables: quotation marks

--- output.tf
+++ output.tf
@@ -1,30 +1,30 @@
-output id {
+output "id" {
   value       = azurerm_network_security_group.nsg.id
   description = "The network security group configuration ID."
 }
-output name {
+output "name" {
   value       = azurerm_network_security_group.nsg.name
   description = "The name of the network security group."
 }
-output resource_group_name {
+output "resource_group_name" {
   value       = azurerm_network_security_group.nsg.resource_group_name
   description = "The name of the resource group in which to create the network security group."
 }
-output location {
+output "location" {
   value       = azurerm_network_security_group.nsg.location
   description = "The location/region where the network security group is created."
 }
-output inbound_rules {
+output "inbound_rules" {
   value       = { for rule in azurerm_network_security_rule.inbound : rule.name => rule }
   description = "Blocks containing configuration of each inbound security rule."
   /*module.MODULE_NAME.inbound_rules["INBOUND_RULE_NAME"].id*/
 }
-output outbound_rules {
+output "outbound_rules" {
   value       = { for rule in azurerm_network_security_rule.outbound : rule.name => rule }
   description = "Blocks containing configuration of each outbound security rule."
   /*module.MODULE_NAME.outbound_rules["OUTBOUND_RULE_NAME"].id*/
 }
-output tags {
-  value       = azurerm_network_security_group.nsg.tags.*
+output "tags" {
+  value       = azurerm_network_security_group.nsg.tags
   description = "The tags assigned to the resource."
 }
