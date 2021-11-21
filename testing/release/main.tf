7f605a1c571d1c2b4f9b63d2b0f114a5c306cc26
Daniel Garrido <gareda95@outlook.es>
2021-11-21 13:28:01 +0100

    Updating the testing system with GitHub Actions

---
+++ testing/release/main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_resource_group" "rg" {
+  name     = uuid()
+  location = "West Europe"
+}
+
+module "network_security_group" {
+  source              = "./terraform-azurerm-network-security-group"
+  name                = azurerm_resource_group.rg.name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  inbound_rules = [
+    {
+      name                       = "inbound_rule_1"
+      priority                   = 100
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_address_prefix      = "10.10.10.10"
+      destination_address_prefix = "10.10.10.10"
+      description                = "inbound_rule_1"
+    },
+    {
+      name                   = "inbound_rule_2"
+      priority               = 200
+      access                 = "Deny"
+      protocol               = "Udp"
+      source_address_prefix  = "20.20.20.20"
+      destination_port_range = 22
+      description            = "inbound_rule_2"
+    }
+  ]
+  outbound_rules = [
+    {
+      name                       = "outbound_rule_1"
+      priority                   = 100
+      access                     = "Allow"
+      protocol                   = "Icmp"
+      source_port_range          = 80
+      destination_address_prefix = "10.10.10.10"
+      description                = "outbound_rule_1"
+    },
+    {
+      name                   = "outbound_rule_2"
+      priority               = 200
+      access                 = "Deny"
+      protocol               = "*"
+      source_port_range      = 443
+      destination_port_range = 3389
+      description            = "outbound_rule_2"
+    }
+  ]
+}
