c8fac3def78e7a935a2dac7e99e0dd21ff43c38b
Daniel Garrido <gareda95@outlook.es>
2021-11-28 09:28:53 +0100

    Internal changes

--- outputs.tf
+++ outputs.tf
@@ -2,28 +2,34 @@ output "id" {
   value       = azurerm_network_security_group.nsg.id
   description = "The network security group configuration ID."
 }
+
 output "name" {
   value       = azurerm_network_security_group.nsg.name
   description = "The name of the network security group."
 }
+
 output "resource_group_name" {
   value       = azurerm_network_security_group.nsg.resource_group_name
   description = "The name of the resource group in which to create the network security group."
 }
+
 output "location" {
   value       = azurerm_network_security_group.nsg.location
   description = "The location/region where the network security group is created."
 }
+
 output "inbound_rules" {
   value       = { for rule in azurerm_network_security_rule.inbound : rule.name => rule }
   description = "Blocks containing configuration of each inbound security rule."
   /*module.MODULE_NAME.inbound_rules["INBOUND_RULE_NAME"].id*/
 }
+
 output "outbound_rules" {
   value       = { for rule in azurerm_network_security_rule.outbound : rule.name => rule }
   description = "Blocks containing configuration of each outbound security rule."
   /*module.MODULE_NAME.outbound_rules["OUTBOUND_RULE_NAME"].id*/
 }
+
 output "tags" {
   value       = azurerm_network_security_group.nsg.tags
   description = "The tags assigned to the resource."
--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
-variable name {
+variable "name" {
   type        = string
   description = "The name of the network security group."
 }
-variable resource_group_name {
+
+variable "resource_group_name" {
   type        = string
   description = "The name of the resource group in which to create the network security group."
 }
-variable location {
+
+variable "location" {
   type        = string
   description = "The location/region where the network security group is created. "
 }
-variable inbound_rules {
+
+variable "inbound_rules" {
   type        = list(map(string))
   default     = []
   description = "List of objects that represent the configuration of each inbound rule."
@@ -28,7 +31,8 @@ variable inbound_rules {
   #   }
   # ]
 }
-variable outbound_rules {
+
+variable "outbound_rules" {
   type        = list(map(string))
   default     = []
   description = "List of objects that represent the configuration of each outbound rule."
@@ -46,7 +50,8 @@ variable outbound_rules {
   #   }
   # ]
 }
-variable tags {
+
+variable "tags" {
   type        = map(string)
   default     = {}
   description = "A mapping of tags to assign to the resource."
